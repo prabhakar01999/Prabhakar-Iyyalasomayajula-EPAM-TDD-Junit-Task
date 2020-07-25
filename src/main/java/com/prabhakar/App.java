86f037469075379d2ebc02d70de40fc83c76110f
prabhakar01999 <prabhakar01999@gmail.com>
2020-07-25 21:15:03 +0530

    Updated App.java

--- src/main/java/com/prabhakar/App.java
+++ src/main/java/com/prabhakar/App.java
@@ -2,5 +2,9 @@ package com.prabhakar;
 
 public class App {
 
-
+    public static void main(String[] args) {
+        String string = "PrabhakarIyyalasomayajula";
+        DeletingAFrom12thPosition deletingAFrom12thPosition = new DeletingAFrom12thPosition();
+        String result = deletingAFrom12thPosition.removeA(string);
+    }
 }
